67ee23a552893e90f42f1f97ffdc0dd54c5805c2
Ivan Wills <ivan.wills@gmail.com>
2012-07-19 21:02:52 +1000

    Upped version

--- lib/Data/Context.pm
+++ lib/Data/Context.pm
@@ -19,7 +19,7 @@ use Path::Class;
 
 use Data::Context::Instance;
 
-our $VERSION     = version->new('0.0.2');
+our $VERSION     = version->new('0.0.3');
 our @EXPORT_OK   = qw//;
 our %EXPORT_TAGS = ();
 
@@ -215,7 +215,7 @@ Data::Context - Configuration data with context
 
 =head1 VERSION
 
-This documentation refers to Data::Context version 0.0.2.
+This documentation refers to Data::Context version 0.0.3.
 
 =head1 SYNOPSIS
 
